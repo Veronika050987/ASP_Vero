9137095079f322b39f93886cc8912d1d12ffdce6
Veronika050987 <veronicaviv@mail.ru>
2026-03-17 21:51:57 +0300

    power, fibonacci done and my svg images added

---
+++ Blazor/Blazor/Components/Pages/PowerCalculator.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazor.Components.Pages
+{
+	public partial class PowerCalculator
+	{
+		private double baseNumber;
+		private double exponent;
+		private double result;
+		private bool isCalculated = false;
+
+		private void Calculate()
+		{
+			result = Math.Pow(baseNumber, exponent);
+			isCalculated = true;
+		}
+	}
+}
---
+++ Blazor/Blazor/Components/Pages/RabbitPyramid.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Numerics;
+
+namespace Blazor.Components.Pages
+{
+	public partial class RabbitPyramid
+	{
+		private int levels = 4; // Количество поколений
+
+		// Модель узла дерева кроликов
+		public class RabbitNode
+		{
+			public int Level { get; set; }
+			public RabbitNode? Left { get; set; }  // Потомки
+			public RabbitNode? Right { get; set; } // Взрослая пара
+		}
+
+		private RabbitNode BuildTree(int currentLevel, int maxLevel)
+		{
+			var node = new RabbitNode { Level = currentLevel };
+			if (currentLevel < maxLevel)
+			{
+				node.Left = BuildTree(currentLevel + 1, maxLevel);
+				// По правилу Фибоначчи, пара кроликов живет и размножается
+				if (currentLevel < maxLevel - 1)
+					node.Right = BuildTree(currentLevel + 1, maxLevel);
+			}
+			return node;
+		}
+	}
+}
