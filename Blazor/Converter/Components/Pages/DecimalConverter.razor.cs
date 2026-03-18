1e43b618110c064f60b0979b48ef2679ed2395c0
Veronika050987 <veronicaviv@mail.ru>
2026-03-18 21:51:24 +0300

    dec 2 bin DONE

---
+++ Blazor/Converter/Components/Pages/DecimalConverter.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Converter.Components.Pages
+{
+	public partial class DecimalConverter
+	{
+		public string ToBinary(int decimalNumber)
+		{
+			return Convert.ToString(decimalNumber, 2);
+		}
+	}
+}
