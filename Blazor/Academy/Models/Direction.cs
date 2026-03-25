a8caa932720f9d65ba37130c0eb4533e74ecf589
Veronika050987 <veronicaviv@mail.ru>
2026-03-25 21:06:53 +0300

    Academy started

---
+++ Blazor/Academy/Models/Direction.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.Models
+{
+	public class Direction
+	{
+		[Key]
+		public byte direction_id { get; set; }
+		public string direction_name { get; set; }
+
+	}
+}
