3a6d18b590ae19fccde740dcb6e8ca71dba2f52e
Veronika050987 <veronicaviv@mail.ru>
2026-04-02 19:52:29 +0300

    index DONE, weekdays for create and edir DONE

---
+++ Blazor/Academy2/Components/Pages/GroupPages/LearningDays.cs
@@ -0,0 +1,24 @@
+﻿namespace Academy2.Components.Pages.GroupPages
+{
+	public class LearningDays
+	{
+		class DayInfo
+		{
+			public string Name { get; set; } = string.Empty;
+			public bool IsWorking { get; set; }
+		}
+
+		public static string GetDays(int Days)
+		{
+			string[] DAYNAMES = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+			List<string> result = new List<string>();
+			for (int i = 0; i < 7; i++)
+			{
+				if (Days % 2 == 1)
+					result.Add(DAYNAMES[i]);
+				Days = Days / 2;
+			}
+			return string.Join(", ", result);
+		}
+	}
+}
