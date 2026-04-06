68c4a4e47d2e9a445e6ad040f50d2cba8307b7dc
Veronika050987 <veronicaviv@mail.ru>
2026-04-06 20:29:07 +0300

    Group list in direction details

--- Blazor/Academy2/Components/Models/Direction.cs
+++ Blazor/Academy2/Components/Models/Direction.cs
@@ -8,6 +8,11 @@ namespace Academy2.Components.Models
 		[Key]
 		[Column(TypeName = "tinyint")]
 		public int direction_id { get; set; }
+		[Required]
 		public string direction_name { get; set; }
+
+		//navigation properties
+
+		public ICollection<Group> Groups { get; set; }
 	}
 }
