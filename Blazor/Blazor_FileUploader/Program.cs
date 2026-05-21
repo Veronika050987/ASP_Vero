77b83a912b227bb41afb59f07ce5aba48d30169c
Veronika050987 <veronicaviv@mail.ru>
2026-05-22 01:37:02 +0300

    still in progress

--- Blazor/Blazor_FileUploader/Program.cs
+++ Blazor/Blazor_FileUploader/Program.cs
@@ -11,6 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
 	.AddInteractiveServerComponents();
 
+builder.Services.AddHttpClient("ApiClient", client =>
+{	
+	client.BaseAddress = new Uri("https://localhost:5000");
+});
+
 builder.Services.AddScoped<HttpClient>();
 
 // === Ваш код для CORS ===
